7571932bb93a3cc1e9936d1ff844b7b6a151eb1c
yexuan <158521138@qq.com>
2019-08-22 21:25:47 +0800

    引用element并搭建路由

---
+++ src/router/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import login from '@/views/login.vue'
+import home from '@/views/home.vue'
+Vue.use(VueRouter)
+export default new VueRouter({
+  routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: home
+    },
+    {
+      name: 'default',
+      path: '/',
+      redirect: { name: 'login' }
+    }
+  ]
+})
